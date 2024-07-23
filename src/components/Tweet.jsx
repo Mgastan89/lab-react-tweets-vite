78d1b5713d9f7a0f34218e7c3033c292883028f5
Magid <mgdagher19@gmail.com>
2024-07-23 15:12:50 -0400

    TweetChanges

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import ProfileImage from './ProfileImage';
 
-
-function Tweet({ tweet}) {
-  console.log("Rendering Tweet with tweet prop:", tweet);
-  return
-
+function Tweet({ tweet }) {
+  return (
+    <div className="tweet">
+      {/* Pass image URL and alt text as props */}
+      <ProfileImage image={tweet.user.image} alt={tweet.user.name} />
+      <div className="body">
+        <div className="top">
+          <span className="name">{tweet.user.name}</span>
+          <span className="handle">@{tweet.user.handle}</span>
+          <span className="timestamp">{tweet.timestamp}</span>
+        </div>
+        <p className="message">{tweet.message}</p>
+        <div className="actions">
+          <i className="fas fa-heart"></i>
+          <i className="fas fa-retweet"></i>
+          <i className="fas fa-reply"></i>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default Tweet;
