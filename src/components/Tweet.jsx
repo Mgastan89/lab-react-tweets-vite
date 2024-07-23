ac65a891bd3ed72078eac11e055296c21c6a8fa4
Magid <mgdagher19@gmail.com>
2024-07-23 16:55:47 -0400

    LetsDoIt

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -3,6 +3,7 @@ import ProfileImage from './ProfileImage';
 import User from './User';
 import Timestamp from './Timestamp';
 import Message from './Message';
+import Actions from './Actions';
 
 function Tweet({ tweet }) {
   return (
@@ -10,20 +11,13 @@ function Tweet({ tweet }) {
       <ProfileImage image={tweet.user.image} alt={tweet.user.name} />
       <div className="body">
         <div className="top">
-
           <User name={tweet.user.name} handle={tweet.user.handle} />
-
           <Timestamp time={tweet.timestamp} />
-        </div>
-
-        <Message message={tweet.message} />
-        <div className="actions">
-          <i className="fas fa-heart"></i>
-          <i className="fas fa-retweet"></i>
-          <i className="fas fa-reply"></i>
+          </div>
+          <Message message={tweet.message} />
+          <Actions />
         </div>
       </div>
-    </div>
   );
 }
 
